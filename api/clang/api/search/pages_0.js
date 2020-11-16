5cb1a43b878fef39e23295542e02816a0d00341e
ziti-ci <ziti-ci@netfoundry.io>
2020-11-16 22:44:39 +0000

    [ci skip] publish docs from CI

--- api/clang/api/search/pages_0.js
+++ api/clang/api/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['building_20the_20project_434',['Building the Project',['../md__ziti-doc_docfx_project_ziti-sdk-c_building.html',1,'']]]
+  ['building_20the_20project_434',['Building the Project',['../md____w_ziti-doc_ziti-doc_docfx_project_ziti-sdk-c_building.html',1,'']]]
 ];
